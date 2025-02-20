51a82b49f7e54deeb93da4a6d98fe667fb290251
sdthompson2 <sdthompson2@ualr.edu>
2025-02-19 21:53:02 -0600

    enum

--- Projects/Project01/main.cpp
+++ Projects/Project01/main.cpp
@@ -3,6 +3,9 @@
 #define ROWS 6
 #define COLS 7
 
+enum GameState { ONGOING, PLAYER_1_WINS, PLAYER_2_WINS, DRAW };
+enum Player { PLAYER_1 = 'X', PLAYER_2 = 'O' };
+
 void makeBoard(char board[ROWS][COLS]) {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
