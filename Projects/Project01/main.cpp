2253913265a546e3070473740d7599cff4f97c90
sdthompson2 <sdthompson2@ualr.edu>
2025-02-19 21:48:35 -0600

    resubmitting gameboard

---
+++ Projects/Project01/main.cpp
@@ -0,0 +1,12 @@
+#include
+
+#define ROWS 6
+#define COLS 7
+
+void makeBoard(char board[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
